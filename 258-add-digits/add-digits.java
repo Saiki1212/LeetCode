24c060045c8b4b66ec860dfc98f5faef1185770e
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2023-12-30 17:20:44 +0530

    Time: 2 ms (84.98%) | Memory: 40.3 MB (20.45%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,7 +1,7 @@
 class Solution {
-    public int addDigits(int n) {
-        if(n == 0) return 0;
-        else if(n % 9 == 0) return 9;
-        else return n % 9;
+    public int addDigits(int num) {
+        if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
     }
 }
