ff53ca1d47d2d53888ec66d8f6c1f47a98b716f6
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-01 12:14:45 +0530

    Time: 1 ms (93.11%) | Memory: 41.7 MB (24.65%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public String largestGoodInteger(String num) {
-        String arr[] = { "999", "888", "777", "666", "555", "444", "333", "222", "111", "000" };
+        String []arr = { "999", "888", "777", "666", "555", "444", "333", "222", "111", "000" };
         for(String s : arr) {
             if(num.indexOf(s) >= 0) return s;
         }
