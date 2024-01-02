85010a301f5854926f364dc71ab662758a96d50e
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-02 18:54:51 +0530

    Time: 2 ms (98.00%) | Memory: 42.5 MB (5.61%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean uniqueOccurrences(int[] nums) {
         int freq[] = new int[2001];
         boolean flag[] = new boolean[2001];
-        int n = nums.length;
         for(int val : nums) 
             freq[val+1000]++;
             
@@ -12,7 +11,7 @@ class Solution {
             if(flag[val]) return false;
             flag[val] = true;
         }
-        
+
         return true;
     }
 }
