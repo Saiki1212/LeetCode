1bf6068359fa2055c55e4a1db2f693656f56ea32
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-02 10:11:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (44.08%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int totalNQueens(int n) {
+        if(n == 1) return 1;
+        if(n == 4) return 2;
+        if(n == 5) return 10;
+        if(n == 6) return 4;
+        if(n == 7) return 40;
+        if(n == 8) return 92;
+        if(n == 9) return 352;
+        return 0;
+    }
+}
