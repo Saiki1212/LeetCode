7718904b9609e65d2a70f7d555c4f0f18d0dd82f
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-02 10:07:13 +0530

    Time: 7 ms (12.08%) | Memory: 44.9 MB (14.62%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,18 +1,24 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        int freq[] = new int[nums.length + 1];
-
-        ArrayList<List<Integer>> ans = new ArrayList<>();
-        for (int c : nums) {
-            if (freq[c] >= ans.size()) {
-                ans.add(new ArrayList<>());
-            }
-
-            // Store the integer in the list corresponding to its current frequency.
-            ans.get(freq[c]).add(c);
-            freq[c]++;
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        int n = nums.length;
+        for(int num : nums) {
+            temp.add(num);
         }
-
-        return ans;
+        int j=0;
+        while(!temp.isEmpty()) {
+            list.add(new ArrayList<>());
+            for(int i=0; i<temp.size(); i++) {
+                if(!list.get(j).contains(temp.get(i))) {
+                    list.get(j).add(temp.get(i));
+                    temp.remove(i);
+                    i--;
+                }
+            }
+            j++;
+            // list.add(new ArrayList(arr));
+        }
+        return list;
     }
 }
