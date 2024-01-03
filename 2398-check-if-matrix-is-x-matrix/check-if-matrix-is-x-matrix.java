988fe14114df9478d8c7681c207878ab8a483e7c
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-03 07:42:32 +0530

    Time: 2 ms (88.24%) | Memory: 45 MB (30.62%) - LeetSync

--- 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.java
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.java
@@ -3,8 +3,8 @@ class Solution {
         int n = grid.length;
         int i = 0, j = n-1;
         while(i<n && j>=0) {
-            if(grid[i][i] == 0) return false;
-            if(grid[i][j] == 0) return false;
+            if(grid[i][i] == 0 || grid[i][j] == 0) return false;
+
             for(int a = 0; a<n; a++) {
                 if(a == i || a == j) continue;
                 if(grid[i][a] != 0) return false;
