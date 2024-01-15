74d2e4c14f0cf9e4ff35eeb2e7e9753a159e77d8
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-15 09:08:12 +0530

    Time: 21 ms (100.00%) | Memory: 91.1 MB (66.70%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,31 +1,38 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        int n = matches.length;
-        List<List<Integer>> list = new ArrayList<>(2);
-        list.add(new ArrayList<>());
-        list.add(new ArrayList<>());
         int[] losses = new int[100001];
 
-        for(int i=0; i<n; i++) {
-            int won = matches[i][0];
-            int lost = matches[i][1];
+        for (int i = 0; i < matches.length; i++) {
+            int win = matches[i][0];
+            int loss = matches[i][1];
 
-            if(losses[won] == 0)
-                losses[won] = -1;
+            if (losses[win] == 0) {
+                losses[win] = -1;
+            } 
 
-            if(losses[lost] == -1)
-                losses[lost] = 1;
-            else
-                losses[lost]++;
+            if (losses[loss] == -1) {
+                losses[loss] = 1;
+            } else {
+                losses[loss]++;
+            }
         }
+
+        List<Integer> zeroLoss = new ArrayList<>();
+        List<Integer> oneLoss = new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
 
         for (int i = 0; i < losses.length; i++) {
             if (losses[i] == -1) {
-                list.get(0).add(i);
+                zeroLoss.add(i);
             } else if (losses[i] == 1) {
-                list.get(1).add(i);
+                oneLoss.add(i);
             }
         }
-        return list;
+
+        result.add(zeroLoss);
+        result.add(oneLoss);
+
+        return result;
     }
 }
