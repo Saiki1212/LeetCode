971eb15df0280c7240fb285e4e60d9d42782723c
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-01-20 15:11:37 +0530

    Time: 2 ms (10.34%) | Memory: 45.4 MB (50.88%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,7 +2,7 @@ class Solution {
     public int search(int[] Arr, int target) {
         int s = 0, e = Arr.length;
         while(s<e) {
-            int m = (s+e)/2;
+            int m = s + (e - s) / 2;
             if(Arr[m] == target)
                 return m;
             else if(Arr[m] < target)
