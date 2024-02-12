d8e843160bc7381f10e4ad3cd483e749f60991bc
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-12 13:04:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (87.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,28 @@
 class Solution {
-    public int[] searchRange(int[] a, int x) {
-        int s = 0, e = a.length-1, val1 = -1;
-      // Floor.......
-      while(s<=e) {
-        int m = (s+e)/2;
-        if(a[m] <= x) {
-          val1 = m;
-          s = m+1;
+    public int[] searchRange(int[] nums, int tar) {
+        int n = nums.length-1, low = 0, up = n+1, s = 0, e = n;
+
+        while(s <= e) {
+            int m = (s+e)/2;
+            if(nums[m] >= tar) {
+                low = m;
+                e = m-1;
+            }
+            else s = m+1;
         }
-        else e = m-1;
-      }
-      if(val1 != -1)
-        val1 = a[val1] == x ? val1 : -1;
+        if(low == n+1 || nums[low] != tar) return new int [] {-1,-1};
 
-      // Ceil......
+        s = 0; e = n;
 
-      s = 0; e = a.length-1;
-      int val = -1;
-
-      while(s<=e) {
-        int m = (s+e)/2;
-        if(a[m] >= x) {
-          val = m;
-          e = m-1;
+        while(s <= e) {
+            int m = (s+e)/2;
+            if(nums[m] > tar) {
+                up = m;
+                e = m-1;
+            }
+            else 
+                s = m+1;
         }
-        else s = m+1;
-      }
-      if(val != -1)
-        val = a[val] == x ? val : -1;
-
-      return new int[]{val, val1};
+        return new int[] {low, up-1};
     }
 }
