cc6dfde330353f238e15397d7046c0b96ba02542
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-13 16:24:37 +0530

    Time: 14 ms (6.91%) | Memory: 44.8 MB (5.70%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,8 +1,24 @@
 class Solution {
     public boolean search(int[] nums, int tar) {
-        int n = nums.length;
-        for(int i=0; i<n; i++)
-            if(nums[i] == tar) return true;
+        int e = nums.length-1, s = 0;
+
+        while(s<=e) {
+            int m = (s+e)/2;
+            System.out.println(m + " -- " + s + " -- " + e);
+            if(nums[m] == tar) return true;
+            if(nums[s] == nums[m] && nums[m] == nums[e]) {
+                s++; e--;
+                continue;
+            }
+            if(nums[m] >= nums[s]) {
+                if(nums[m] > tar && nums[s] <= tar) e = m-1;
+                else s = m+1;
+            }
+            else {
+                if(nums[m] < tar && nums[e] >= tar) s = m+1;
+                else e = m-1;
+            }
+        }
         return false;
     }
 }
