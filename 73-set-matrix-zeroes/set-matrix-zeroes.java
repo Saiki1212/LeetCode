f530e45928ac2b0b1fb77615ec8e56f9ad49a1c3
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-18 16:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (10.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,28 +2,28 @@ class Solution {
 
     public void setZeroes(int[][] mat) {
         int row = mat.length, col = mat[0].length;
-        boolean [][] check = new boolean [row][col];
+        boolean [] checkrow = new boolean [row];
+        boolean [] checkcol = new boolean [col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (mat[i][j] == 0) {
-                    check[i][j] = true;
+                    checkrow[i] = true;
+                    checkcol[j] = true;
                 }
             }
         }
 
         for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (check[i][j]) {
+            if(checkrow[i]) {
+                for(int j = 0; j<col; j++)
                     mat[i][j] = 0;
-                    for(int a = 0; a < row; a++) {
-                        mat[a][j] = 0;
-                        // check[a][j] = false;
-                    }
-                    for(int a = 0; a < col; a++) {
-                        mat[i][a] = 0;
-                        // check[i][a] = false;
-                    }
-                }
+            }
+        }
+
+        for (int i = 0; i < col; i++) {
+            if(checkcol[i]) {
+                for(int j = 0; j<row; j++)
+                    mat[j][i] = 0;
             }
         }
     }
