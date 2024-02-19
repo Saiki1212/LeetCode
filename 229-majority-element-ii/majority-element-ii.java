bf71e875d6305ad100e247295ba60521d53f0f49
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-19 17:21:27 +0530

    Time: 2 ms (99.31%) | Memory: 48 MB (50.06%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,13 +1,28 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        int great = n/3;
-        List<Integer> list = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i : nums)  map.put(i, map.getOrDefault(i, 0) + 1);
-        map.forEach((key, value) -> {
-            if(value > great) list.add(key);
-        });
+        List<Integer> list = new ArrayList<>(2);
+        int c1 = 0, c2 = 0, el1 = Integer.MAX_VALUE, el2 = el1;
+        for(int i : nums)  {
+            if(c1 == 0 && i != el2) {
+                c1 = 1; el1 = i;
+            }
+            else if(c2 == 0 && i != el1) {
+                c2 = 1; el2 = i;
+            }
+            else if(el1 == i) c1++;
+            else if(el2 == i) c2++;
+            else {
+                c1--; c2--;
+            }
+        }
+        c1 = 0; c2 = 0;
+        for(int i : nums) {
+            if(i == el1) c1++;
+            else if(i == el2) c2++;
+        }
+        if(c1 > n/3) list.add(el1);
+        if(c2 > n/3) list.add(el2);
         return list;
     }
 }
