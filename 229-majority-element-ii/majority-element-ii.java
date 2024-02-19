817ab8d88c83245c715bb6e2a09d6a5b6160ac62
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-19 16:56:11 +0530

    Time: 13 ms (27.14%) | Memory: 47.2 MB (95.82%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,56 +1,14 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int count1 = 0, count2 = 0; // Counters for the potential majority elements
-        int candidate1 = 0, candidate2 = 0; // Potential majority element candidates
-
-        // First pass to find potential majority elements.
         int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            // If count1 is 0 and the current number is not equal to candidate2, update candidate1.
-            if (count1 == 0 && nums[i] != candidate2) {
-                count1 = 1;
-                candidate1 = nums[i];
-            } 
-            // If count2 is 0 and the current number is not equal to candidate1, update candidate2.
-            else if (count2 == 0 && nums[i] != candidate1) {
-                count2 = 1;
-                candidate2 = nums[i];
-            } 
-            // Update counts for candidate1 and candidate2.
-            else if (candidate1 == nums[i]) {
-                count1++;
-            } else if (candidate2 == nums[i]) {
-                count2++;
-            } 
-            // If the current number is different from both candidates, decrement their counts.
-            else {
-                count1--;
-                count2--;
-            }
+        int great = n/3;
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i : nums)  map.put(i, map.getOrDefault(i, 0) + 1);
+        for(int i : nums) {
+            int val = map.get(i);
+            if(val > great && !list.contains(i)) list.add(i); 
         }
-
-        List<Integer> result = new ArrayList<>();
-        int threshold = n / 3; // Threshold for majority element
-
-        // Second pass to count occurrences of the potential majority elements.
-        count1 = 0;
-        count2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (candidate1 == nums[i]) {
-                count1++;
-            } else if (candidate2 == nums[i]) {
-                count2++;
-            }
-        }
-
-        // Check if the counts of potential majority elements are greater than n/3 and add them to the result.
-        if (count1 > threshold) {
-            result.add(candidate1);
-        }
-        if (count2 > threshold) {
-            result.add(candidate2);
-        }
-
-        return result;
+        return list;
     }
 }
