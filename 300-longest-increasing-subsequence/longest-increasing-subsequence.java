05d3911eef516b61c057d46b9c51576bc3f04e87
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-02-24 11:48:13 +0530

    Time: 36 ms (70.55%) | Memory: 44.6 MB (18.21%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,17 @@
-public class Solution {
+class Solution {
     public int lengthOfLIS(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int maxi = 1;
+        int dp[] = new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1; i<n; i++) {
+            for(int j = 0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
                 }
             }
         }
-
-        int maxLength = Arrays.stream(dp).max().orElse(0);
-        return maxLength;
+        for(int i : dp) maxi = Math.max(maxi, i);
+        return maxi;
     }
-} 
+}
