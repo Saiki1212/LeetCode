86da5a9e217dc428f8bdd64d15859ca02881b7db
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-03-09 19:42:34 +0530

    Time: 2 ms (81.56%) | Memory: 62.1 MB (92.22%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -5,8 +5,8 @@ class Solution {
 
         while(i < nums1.length && j < nums2.length) {
             if(nums1[i] == nums2[j]) return nums1[i];
-            else if (nums1[i] > nums2[j])j++;
-            else i++;
+            else if (nums1[i] < nums2[j]) i++;
+            else j++;
         }
         return -1;
     }
