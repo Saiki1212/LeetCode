1fef361489c72baa883597e9b830d887ab63d347
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-22 11:36:25 +0530

    Time: 1 ms (72.75%) | Memory: 45 MB (98.02%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,37 +1,31 @@
 class Solution {
-
     public void setZeroes(int[][] mat) {
-        int row = mat.length, col = mat[0].length, col0 = 3;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (mat[i][j] == 0) {
-                    mat[i][0] = 0;
-                    if(j != 0) mat[0][j] = 0;
-                    else col0 = 0;
-                }
-            }
-        }
+        int row = mat.length, col = mat[0].length;
 
-        for (int i = 1; i < row; i++) {
-            for(int j = 1; j<col; j++) {
-                if(mat[i][j] != 0) {
-                    if (mat[i][0] == 0  || mat[0][j] == 0 ) {
-                        mat[i][j] = 0 ;
+        int large = Integer.MIN_VALUE-10;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(mat[i][j] == 0) {
+                    for(int a = 0; a<row; a++) {
+                        if(mat[a][j] != 0)
+                        mat[a][j] = large;
+                    }
+                    for(int a = 0; a<col; a++) {
+                        if(mat[i][a] != 0)
+                        mat[i][a] = large;
                     }
                 }
             }
         }
-        
-        if(mat[0][0] == 0) {
-            for(int i = 0; i<col; i++) {
-                mat[0][i] = 0;
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(mat[i][j] == large) {
+                    mat[i][j] = 0;
+                }
             }
         }
-        if(col0 == 0) {
-            for(int i = 0; i<row; i++) {
-                mat[i][0] = 0;
-            }
-        }
-        
+
     }
 }
