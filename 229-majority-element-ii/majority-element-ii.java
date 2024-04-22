5989fe44c5a7f4276efc20b16d0d40b34e27fccd
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-22 16:19:04 +0530

    Time: 2 ms (99.41%) | Memory: 47.9 MB (51.36%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,28 +1,31 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        List<Integer> list = new ArrayList<>(2);
-        int c1 = 0, c2 = 0, el1 = Integer.MAX_VALUE, el2 = el1;
-        for(int i : nums)  {
-            if(c1 == 0 && i != el2) {
-                c1 = 1; el1 = i;
+        List<Integer> list = new ArrayList<>();
+        
+        int c1, c2, e1, e2;
+        c1 = c2 = 0;
+        e1 = e2 = Integer.MAX_VALUE;
+
+        for(int val : nums) {
+            if(c1 == 0 && val != e2) {
+                e1 = val; c1++;
             }
-            else if(c2 == 0 && i != el1) {
-                c2 = 1; el2 = i;
+            else if(c2 == 0 && val != e1) {
+                e2 = val; c2++;
             }
-            else if(el1 == i) c1++;
-            else if(el2 == i) c2++;
-            else {
-                c1--; c2--;
-            }
+            else if(val == e1) c1++;
+            else if(val == e2) c2++;
+            else {c1--; c2--;}
         }
-        c1 = 0; c2 = 0;
-        for(int i : nums) {
-            if(i == el1) c1++;
-            else if(i == el2) c2++;
+        c1 = c2 = 0;
+        for(int val : nums) {
+            if(val == e1) c1++;
+            else if(val == e2) c2++;
         }
-        if(c1 > n/3) list.add(el1);
-        if(c2 > n/3) list.add(el2);
+        if(c1 > n/3) list.add(e1);
+        if(c2 > n/3) list.add(e2);
+
         return list;
     }
 }
