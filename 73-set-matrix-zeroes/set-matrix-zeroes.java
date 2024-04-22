1c4e1bb81b694e8d83bd3e414088f91747e86144
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-22 11:38:57 +0530

    Time: 1 ms (72.75%) | Memory: 45.5 MB (38.46%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,26 +2,20 @@ class Solution {
     public void setZeroes(int[][] mat) {
         int row = mat.length, col = mat[0].length;
 
-        int large = Integer.MIN_VALUE-10;
+        int r[] = new int[row], c[] = new int[col];
 
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
                 if(mat[i][j] == 0) {
-                    for(int a = 0; a<row; a++) {
-                        if(mat[a][j] != 0)
-                        mat[a][j] = large;
-                    }
-                    for(int a = 0; a<col; a++) {
-                        if(mat[i][a] != 0)
-                        mat[i][a] = large;
-                    }
+                    r[i] = 1;
+                    c[j] = 1;
                 }
             }
         }
 
         for(int i=0; i<row; i++) {
             for(int j=0; j<col; j++) {
-                if(mat[i][j] == large) {
+                if(r[i] == 1 || c[j] == 1) {
                     mat[i][j] = 0;
                 }
             }
