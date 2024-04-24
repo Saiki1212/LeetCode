9517b8736888130a540da81c2bf418fd2564105b
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-24 12:29:56 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (83.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,22 +1,16 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        int s = 1, e = n-2;
+        int s = 0, n = nums.length, e=n-1;
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
-
+        
         while(s <= e) {
             int m = (s+e)/2;
-            if(nums[m] == nums[m-1]){
-                if(m % 2 == 0) e = m-1;
-                else s = m+1;
-            }
-            else if(nums[m] == nums[m+1]) {
-                if(m % 2 == 0) s = m+1;
-                else e = m-1;
-            }
-            else return nums[m];
+            if(nums[m] != nums[m-1] && nums[m] != nums[m+1] ) return nums[m];
+            if(m % 2 == 0 && nums[m-1] == nums[m]) e = m-1;
+            else if(m % 2 != 0 && nums[m+1] == nums[m]) e = m-1;
+            else s=m+1;
         }
         return -1;
     }
