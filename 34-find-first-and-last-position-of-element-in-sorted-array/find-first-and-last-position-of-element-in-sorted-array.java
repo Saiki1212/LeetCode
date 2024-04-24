1f1b6e29e913ac90f6795615470d1dc0aeb52683
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-24 11:06:39 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (42.30%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] searchRange(int[] nums, int tar) {
-        int n = nums.length-1, low = 0, up = n+1, s = 0, e = n;
+        int res[] = new int[2];
+        res[0] = -1; res[1] = -1;
+        int n = nums.length;
+        if(n == 0) return res;
+        int s = 0, e = n-1;
 
         while(s <= e) {
             int m = (s+e)/2;
-            if(nums[m] >= tar) {
-                low = m;
-                e = m-1;
-            }
-            else s = m+1;
+            if(nums[m] < tar) s = m+1;
+            else e = m-1;
         }
-        if(low == n+1 || nums[low] != tar) return new int [] {-1,-1};
+        if(s == n || nums[s] != tar) return res;
+        if(nums[s] == tar) res[0] = s;
 
-        s = 0; e = n;
-
+        s = 0; e = nums.length-1;
         while(s <= e) {
             int m = (s+e)/2;
-            if(nums[m] > tar) {
-                up = m;
-                e = m-1;
-            }
-            else 
-                s = m+1;
+            if(nums[m] <= tar) s = m+1;
+            else e = m-1;
         }
-        return new int[] {low, up-1};
+        if(nums[e] == tar) res[1] = e;
+        return res;
     }
 }
