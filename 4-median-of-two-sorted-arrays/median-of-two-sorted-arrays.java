91106d70b4e55684bef6bbe431dd2836015e0ab8
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-25 11:43:47 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (18.24%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,34 +1,28 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
+        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
         int len = n1+n2;
-        int idx2 = len/2, idx1 = (len/2)-1, ele1 = 0, ele2 = 0;
+        int s = 0, e = n1;
 
-        int i = 0, j = 0, k = 0, pre = 0;
-        while( i<n1 && j<n2 ) {
-            if(nums1[i] < nums2[j]) pre = nums1[i++];
-            else pre = nums2[j++];
+        
+        int mid = (n1+n2+1)/2;
+        while( s<=e ) {
+            int mid1 = (s+e)/2;
+            int mid2 = mid-mid1;
+            
+            int l1 = (mid1 > 0) ? nums1[mid1-1] : Integer.MIN_VALUE;
+            int l2 = (mid2 > 0) ? nums2[mid2-1] : Integer.MIN_VALUE;
+            int r1 = (mid1 < n1) ? nums1[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2 < n2) ? nums2[mid2] : Integer.MAX_VALUE;
 
-            if(k == idx1) ele1 = pre;
-            if(k == idx2) ele2 = pre;
-            k++;
-        }
-        while(i<n1) {
-            pre = nums1[i++];
-            if(k == idx1) ele1 = pre;
-            if(k == idx2) ele2 = pre;
-            k++;
-        }
-        while(j<n2) {
-            pre = nums2[j++];
-            if(k == idx1) ele1 = pre;
-            if(k == idx2) ele2 = pre;
-            k++;
-        }
-
-        if(len % 2 == 0) {
-            return (double) (ele1+ele2)/2.0;
-        }
-        return ele2;
+            if(l1 <= r2 && l2 <= r1){
+                if(len % 2 != 0) return (double) Math.max(l1, l2);
+                return ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            }
+            else if(l1 > r2) e = mid1 - 1;
+            else s = mid1 + 1;
+        } 
+        return 0;
     }
 }
