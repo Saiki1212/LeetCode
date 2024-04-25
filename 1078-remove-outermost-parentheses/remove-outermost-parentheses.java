a094bb7b49c3581053943d78ee37f9e413888c1a
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-25 22:42:48 +0530

    Time: 1 ms (100.00%) | Memory: 42.1 MB (64.33%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,15 +1,23 @@
 class Solution {
     public String removeOuterParentheses(String s) {
         int n = s.length();
-        int count = 1;
-        String res = "";
-        for(int i=1; i<n; i++) {
-            char c=s.charAt(i);
-            if(c == '(') count++;
-            else count--;
-            if(count == 0) {i++; count++;}
-            else res += c;
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i = 0; i<n;) {
+            i = checkSubString(i, s, n, sb);
         }
-        return res;
+        return sb.toString();
+    }
+
+    public static int checkSubString(int i, String s, int n, StringBuilder sb) {
+        int cnt = 1, idx = i+1;
+
+        while(cnt > 0 && idx < n) {
+            if(s.charAt(idx) == '(') cnt++;
+            else cnt--;
+            idx++;
+        }
+        sb.append(s.substring(i+1, idx-1));
+        return idx;
     }
 }
