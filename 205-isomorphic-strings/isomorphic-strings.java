4a05663b91579b4329a22fd96291d91e91902c54
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-26 09:03:57 +0530

    Time: 9 ms (64.27%) | Memory: 41.8 MB (96.41%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -4,21 +4,16 @@ class Solution {
         if(n1 != n2) return false;
         
         Map<Character, Character> map = new HashMap<>();
-        Map<Character, Character> map1 = new HashMap<>();
 
         for(int i = 0; i<n1; i++) {
             char a = s.charAt(i), b = t.charAt(i);
             if(map.containsKey(a)) {
                 if(map.get(a) != b) return false;
             }
-            else map.put(a,b);
-        }
-        for(int i = 0; i<n1; i++) {
-            char b = s.charAt(i), a = t.charAt(i);
-            if(map1.containsKey(a)) {
-                if(map1.get(a) != b) return false;
+            else {
+                if(map.containsValue(b)) return false;
+                map.put(a, b);
             }
-            else map1.put(a,b);
         }
         return true;
     }
