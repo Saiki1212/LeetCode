e99cc67ca79b2f8e5eeb29c8a769f83179eef20d
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-26 08:56:26 +0530

    Time: 12 ms (40.62%) | Memory: 42.3 MB (61.92%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -3,13 +3,22 @@ class Solution {
         int n1 = s.length(), n2 = t.length();
         if(n1 != n2) return false;
         
-        int []freq1 = new int[257];
-        int []freq2 = new int[257];
+        Map<Character, Character> map = new HashMap<>();
+        Map<Character, Character> map1 = new HashMap<>();
 
-        for(int i=0; i<n1; i++) {
-            if(freq1[s.charAt(i)] != freq2[t.charAt(i)]) return false;
-            freq1[s.charAt(i)] = i+1;
-            freq2[t.charAt(i)] = i+1;
+        for(int i = 0; i<n1; i++) {
+            char a = s.charAt(i), b = t.charAt(i);
+            if(map.containsKey(a)) {
+                if(map.get(a) != b) return false;
+            }
+            else map.put(a,b);
+        }
+        for(int i = 0; i<n1; i++) {
+            char b = s.charAt(i), a = t.charAt(i);
+            if(map1.containsKey(a)) {
+                if(map1.get(a) != b) return false;
+            }
+            else map1.put(a,b);
         }
         return true;
     }
