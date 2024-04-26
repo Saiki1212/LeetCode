e4f751141d8543dc6c6b2dd7dd6278985045960c
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-26 10:43:07 +0530

    Time: 373 ms (13.31%) | Memory: 45.5 MB (58.90%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,29 +1,27 @@
 class Solution {
     public String frequencySort(String s) {
         int n = s.length();
-        Map<Character, Integer> map = new HashMap<>();
+        char freq[] = new char[123];
+
         for(int i=0; i<n; i++) {
-            char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0)+1);
+            freq[s.charAt(i)]++;
         }
+
         String res = "";
-        List<Character> list = new ArrayList<>();
-        for(Map.Entry<Character, Integer> e : map.entrySet()) {
-            int maxi = e.getValue();
-            
-            char a = e.getKey();
-            // System.out.println(a + "   -   " + maxi);
-            if(list.contains(a)) maxi = 0;
-            
-            for(Map.Entry<Character, Integer> f : map.entrySet()) {
-                if(maxi < f.getValue() && !list.contains(f.getKey())) {
-                    maxi = f.getValue();
-                    a = f.getKey();
+
+        for(int i=0; i<123; i++) {
+            char a = (char)i;
+            int maxi = freq[a];
+
+            for(int j=0; j<123; j++) {
+                char c = (char)j;
+                if(maxi < freq[c]) {
+                    maxi = freq[c];
+                    a = c;
                 }
             }
-            // System.out.println(a + "   -   " + maxi + "\n");
-            list.add(a);
-            for(int i=0; i<maxi; i++) res += a;
+            freq[a] = 0;
+            for(int k=0; k<maxi; k++) res+=a;
         }
         return res;
     }
