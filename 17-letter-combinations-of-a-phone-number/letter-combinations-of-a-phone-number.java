5d061993b915f169d10ab99ea752aee7898cecad
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-29 18:47:18 +0530

    Time: 5 ms (38.60%) | Memory: 42.3 MB (26.42%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,12 +1,12 @@
 class Solution {
 
-    public static void GetPatterns(List<String> list, String[] nums, String s, String digits, int i) {
+    public static void GetPatterns(List<String> list, Map<Character, String> nums, String s, String digits, int i) {
         if(i == digits.length()) {
             list.add(s);
             return;
         }
 
-        String curr = nums[digits.charAt(i)-'0'-2];
+        String curr = nums.get(digits.charAt(i));
 
         for(int j = 0; j<curr.length(); j++) {
             GetPatterns(list, nums, s+curr.charAt(j), digits, i+1);
@@ -19,7 +19,15 @@ class Solution {
             return list;
         }
 
-        String nums[] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        Map<Character, String> nums = new HashMap<>();
+        nums.put('2', "abc");
+        nums.put('3', "def");
+        nums.put('4', "ghi");
+        nums.put('5', "jkl");
+        nums.put('6', "mno");
+        nums.put('7', "pqrs");
+        nums.put('8', "tuv");
+        nums.put('9', "wxyz");
 
         GetPatterns(list, nums, "", digits, 0);
     
