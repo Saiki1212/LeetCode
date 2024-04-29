db414fa7425cc150240d10e7a6a7246e712315a4
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-04-29 15:55:22 +0530

    Time: 1 ms (99.75%) | Memory: 43.5 MB (27.65%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,10 +1,11 @@
 class Solution {
 
     public static void PowerSet(List<List<Integer>> list, List<Integer> temp, int[] nums, int i, int n) {
-        if(!list.contains(temp))
-            list.add(new ArrayList<>(temp));
+        
+        list.add(new ArrayList<>(temp));
+        
         for(int j = i; j<n; j++) {
-            // if(j > i && nums[j] == nums[j-1]) continue;
+            if(j > i && nums[j] == nums[j-1]) continue;
             temp.add(nums[j]);
             PowerSet(list, temp, nums, j+1, n);
             temp.remove(temp.size()-1);
