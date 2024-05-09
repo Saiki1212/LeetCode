dcce5609d71e67e1d73df829f25e60a6041ff664
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-05-09 10:09:45 +0530

    Time: 34 ms (97.44%) | Memory: 61.3 MB (77.94%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.java
@@ -4,11 +4,9 @@ class Solution {
         Arrays.sort(nums);
         long sum = 0;
         int i = n-1, minus = 0;
-        while( k > 0 ) {
-            nums[i] = Math.max(nums[i]-minus, 0);
+        while( k-- > 0 ) {
+            nums[i] = Math.max(nums[i]-minus++, 0);
             sum += nums[i--];
-            minus++;
-            k--;
         }
         return sum;
     }
