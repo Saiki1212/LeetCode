bd16af53f7c90e23268fb02002b0c00e5af1459c
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-19 12:15:18 +0530

    Time: 87 ms (5.85%) | Memory: 52.8 MB (7.40%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,44 +1,41 @@
 class Solution {
-    public int sumSubarrayMins(int[] arr) {
-        int length = arr.length;
-        int[] left = new int[length];
-        int[] right = new int[length];
-      
-        Arrays.fill(left, -1);
-        Arrays.fill(right, length);
-      
-        Deque<Integer> stack = new ArrayDeque<>();
-      
-        for (int i = 0; i < length; ++i) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
-                stack.pop();
+    public int sumSubarrayMins(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+
+        // Elements smaller than nums[i] in right and left;
+        int [] key1 = new int[n];
+        int [] key2 = new int[n];
+
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
             }
-            if (!stack.isEmpty()) {
-                left[i] = stack.peek();
-            }
-            stack.push(i);
+            if(st.isEmpty()) key2[i] = n-i;
+            else key2[i] = st.peek()-i;
+            // System.out.println("nums[i] ==  " +(nums[i])+ "  --  key2[i] = =  "+key[i]);
+            st.push(i);
         }
-      
-        stack.clear();
-      
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
+
+        st.clear();
+
+        int mod = 1000000007;
+
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
             }
-            if (!stack.isEmpty()) {
-                right[i] = stack.peek();
-            }
-            stack.push(i);
+            if(st.isEmpty()) key1[i] = i+1;
+            else  key1[i] = i-st.peek();
+            // System.out.println("nums[i] ==  " +(nums[i])+ "  --  key1[i] = =  "+key[i]);
+            st.push(i);
         }
-      
-        int mod = (int) 1e9 + 7;
-        long answer = 0;
-      
-        for (int i = 0; i < length; ++i) {
-            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
-            answer %= mod;
+
+        long t = 0;
+        for(int i=0; i<n; i++) {
+            long c = key1[i] * key2[i] % mod;
+            t = (t + (nums[i]*c) % mod)%mod;
         }
-      
-        return (int) answer;
+        return (int)t;
     }
 }
