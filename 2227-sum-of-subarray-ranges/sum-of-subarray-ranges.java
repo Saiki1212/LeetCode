a3c00accf6c7be183c63099eabfaf8ccac09b833
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-19 15:11:01 +0530

    Time: 32 ms (13.37%) | Memory: 45 MB (8.89%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,37 +1,76 @@
 class Solution {
-
-    public static int FindMaxMin(int[] nums, int i, int j, boolean isMax) {
-        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-        for(; i<=j; i++) {
-                max = Math.max(max, nums[i]);
-                min = Math.min(min, nums[i]);
-        }
-        if(isMax) return max;
-        return min;
-    }
-
     public long subArrayRanges(int[] nums) {
         int n = nums.length;
+        Stack<Integer> st = new Stack<>();
 
-        long res = 0;
-        for(int i=1; i<n; i++) {
-            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-            int j = 0;
-            for(; j<=i; j++) {
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
+        // sub array minimuns....
+        int [] key1 = new int[n];
+        int [] key2 = new int[n];
+
+        long subArrayMinimums = FindMinimum(st, key1, key2, n, nums);
+        Arrays.fill(key1, 0);
+        Arrays.fill(key2, 0);
+        st.clear();
+        long subArrayMaximums = FindMaximum(st, key1, key2, n, nums);
+        return subArrayMaximums - subArrayMinimums;
+
+    }
+
+    public static long FindMinimum(Stack<Integer> st, int [] key1, int [] key2, int n, int[] nums) {
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && nums[st.peek()] >= nums[i]) {
+                st.pop();
             }
-            int diff = max-min;
-            res += diff;
-            for(; j<n; j++) {
-                if(nums[j-i-1] == max) max = FindMaxMin(nums, (j-i), j, true);
-                else if(nums[j-i-1] == min) min = FindMaxMin(nums, (j-i), j, false);
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
-                diff = max-min;
-                res += diff;
-            }
+            if(st.isEmpty()) key2[i] = n-i;
+            else key2[i] = st.peek()-i;
+            st.push(i);
         }
-        return res;
+
+        st.clear();
+
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && nums[st.peek()] > nums[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) key1[i] = i+1;
+            else  key1[i] = i-st.peek();
+            st.push(i);
+        }
+
+        long t = 0;
+        for(int i=0; i<n; i++) {
+            long c = key1[i] * key2[i] ;
+            t = t + nums[i]*c;
+        }
+        return t;
+    }
+
+    public static long FindMaximum(Stack<Integer> st, int [] key1, int [] key2, int n, int[] nums) {
+        for(int i=n-1; i>=0; i--) {
+            while(!st.isEmpty() && nums[st.peek()] <= nums[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) key2[i] = n-i;
+            else key2[i] = st.peek()-i;
+            st.push(i);
+        }
+
+        st.clear();
+
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && nums[st.peek()] < nums[i]) {
+                st.pop();
+            }
+            if(st.isEmpty()) key1[i] = i+1;
+            else  key1[i] = i-st.peek();
+            st.push(i);
+        }
+
+        long t = 0;
+        for(int i=0; i<n; i++) {
+            long c = key1[i] * key2[i] ;
+            t = t + nums[i]*c ;
+        }
+        return t;
     }
 }
