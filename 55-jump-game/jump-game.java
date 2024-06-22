d7aa17feb7ff3c9d6114a2b9de31dadd1113aad7
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-22 21:43:13 +0530

    Time: 2 ms (79.91%) | Memory: 45.7 MB (24.34%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -4,7 +4,7 @@ class Solution {
         int maxi = 0;
         for(int i=0; i<n; i++) {
             if(maxi < i) return false;
-            else maxi = Math.max(maxi, nums[i] + i);
+            maxi = Math.max(maxi, nums[i] + i);
 
             if(maxi >= n-1) return true;
         }
