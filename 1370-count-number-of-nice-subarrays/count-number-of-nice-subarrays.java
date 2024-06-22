49f7628a9c745cd2a1f35dc4c6b6b49e1b7ddfdf
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-22 09:42:16 +0530

    Time: 14 ms (37.59%) | Memory: 54.7 MB (71.73%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,26 +1,23 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
+        return find(nums, nums.length, k) - find(nums, nums.length, k-1);
+    }
+
+    public static int find(int [] nums, int n, int k) {
+        int odds = 0;
         int count = 0;
-        int right = 0;
-        int oddCount = 0;
         
-        int[] prefixCounts = new int[nums.length + 1];
-        prefixCounts[0] = 1; 
-        
-        while (right < nums.length) {
-            if (nums[right] % 2 != 0) {
-                oddCount++;
+        int i = 0, j = 0;
+
+        while(i<n) {
+            if(nums[i] % 2 != 0) odds++;
+            while(odds > k && j<i) {
+                if(nums[j] % 2 != 0) odds--;
+                j++;
             }
-            
-            if (oddCount >= k) {
-                count += prefixCounts[oddCount - k];
-            }
-            
-            prefixCounts[oddCount]++;
-            
-            right++;
+            if(odds <= k) count += i-j+1;
+            i++;
         }
-        
         return count;
     }
 }
