937d6c237f6ca15d3d457a46cd83002ccabc4fa6
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-23 15:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (67.71%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,22 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+
+    public static void find(List<Integer> list, TreeNode root, int level) {
+        if(root == null) return;
+        if(list.size() == level) {
+            list.add(root.val);
+        }
+        find(list, root.right, level+1);
+        find(list, root.left, level+1);
+    }
+
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         if(root == null) return list;
 
-        Deque<TreeNode> dq = new ArrayDeque<>();
-        dq.add(root);
-
-        while(!dq.isEmpty()) {
-            int n = dq.size();
-
-            for(int i=0; i<n; i++) {
-                TreeNode node = dq.pollFirst();
-                if(node.left != null) dq.add(node.left);
-                if(node.right != null) dq.add(node.right);
-                if(i == n-1) list.add(node.val);
-            }
-
-        }
+        find(list, root, 0);
         return list;
     }
 }
