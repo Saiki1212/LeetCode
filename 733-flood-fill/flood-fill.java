5f489b2e556431cb1f9e1102cc86860416c7787f
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-25 14:44:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (92.58%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,18 @@
 class Solution {
     public int[][] floodFill(int[][] image, int x, int y, int color) {
-        // if(color == 0) return image;
+        if(image[x][y] == color) return image;
         
-        changeColor(image, x, y, new int[]{-1, 0, 0, 1}, new int[]{0, -1, 1, 0});
-        for(int i = 0; i<image.length; i++) {
-            for(int j = 0; j<image[0].length; j++) {
-                if(image[i][j] == -1) image[i][j] = color;
-            }
-        }
-
+        changeColor(image, x, y, color, image[x][y]);
         return image;
     }
 
-    public static void changeColor(int[][] image, int x, int y, int [] rowMat, int [] colMat) {
-        int currentValue = image[x][y];
-        image[x][y] = -1;
-        for(int i = 0; i<4; i++) {
-            int xx = x + rowMat[i];
-            int yy = y + colMat[i];
-            if(xx < 0 || yy < 0 || xx >= image.length || yy >= image[0].length || image[xx][yy] != currentValue) continue;
-            changeColor(image, xx, yy, rowMat, colMat);
-        }
+    public static void changeColor(int[][] image, int x, int y, int color, int ini) {
+        if(x < 0 || y < 0 || x >= image.length || y >= image[0].length || image[x][y] != ini) return;
+        image[x][y] = color;
+        changeColor(image, x-1, y, color, ini);
+        changeColor(image, x, y-1, color, ini);
+        changeColor(image, x, y+1, color, ini);
+        changeColor(image, x+1, y, color, ini);
     }
 
 }
