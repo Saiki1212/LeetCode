f291a6680c0bd2008d6a9a70cc477c0a615bf7ec
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-26 09:05:21 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (99.93%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -17,7 +17,13 @@ class Solution {
 
     public static TreeNode find(ArrayList<Integer> list, int s, int e) {
         if(s > e) return null;
-        int mid = (s+e)/2;
+        if (s == e) {
+            TreeNode ret = new TreeNode(list.get(s));
+            ret.left = null;
+            ret.right = null;
+            return ret;
+        }
+        int mid = s + (e-s)/2;
         TreeNode root = new TreeNode(list.get(mid));
         root.left = find(list, s, mid-1);
         root.right = find(list, mid+1, e);
