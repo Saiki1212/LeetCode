9e3674df67b8f4520225ca47e79b3018df59d8cc
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-29 07:31:25 +0530

    Time: 54 ms (86.91%) | Memory: 65.1 MB (68.45%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            res.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
+        }
+
+        // Build the adjacency list
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+        }
+
+        // Perform DFS to find all ancestors for each node
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[n];
+            dfs(i, i, adj, res, visited);
+        }
+
+        // Sort the ancestor lists
+        for (List<Integer> list : res) {
+            Collections.sort(list);
+        }
+
+        return res;
+    }
+
+    private void dfs(int start, int current, List<List<Integer>> adj, List<List<Integer>> res, boolean[] visited) {
+        for (int neighbor : adj.get(current)) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                res.get(neighbor).add(start);
+                dfs(start, neighbor, adj, res, visited);
+            }
+        }
+    }
+}
