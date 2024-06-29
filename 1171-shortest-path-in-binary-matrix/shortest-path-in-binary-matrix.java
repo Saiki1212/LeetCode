3812287e3c07cf28901e508c28f9d96aca417419
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-29 21:30:00 +0530

    Time: 32 ms (17.35%) | Memory: 45.4 MB (47.19%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    class Pair{
+        int x, y, w;
+        Pair(int _x, int _y, int _w) {
+            x = _x;
+            y = _y;
+            w = _w;
+        }
+    }
+
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int r = grid.length;
+        if(grid[0][0] == 1 || grid[r-1][r-1] == 1) return -1;
+
+        boolean [][] visited = new boolean[r][r];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.w - b.w);
+        pq.add(new Pair(0, 0, 1));
+        visited[0][0] = true;
+
+        while( !pq.isEmpty() ) {
+            Pair p = pq.remove();
+            if(p.x == r-1 && p.y == r-1) return p.w;
+            int drow[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int dcol[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+            for(int i = 0; i<8; i++) {
+                int xx = drow[i] + p.x, yy = dcol[i] + p.y;
+                if( xx < 0 || yy < 0 || xx >= r || yy >= r || grid[xx][yy] == 1 || visited[xx][yy] ) continue;
+                pq.add(new Pair(xx, yy, p.w+1));
+                visited[xx][yy] = true;
+            }
+        }
+        return -1;
+    }
+}
