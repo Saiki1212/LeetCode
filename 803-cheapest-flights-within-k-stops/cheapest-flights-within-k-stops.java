983714ed03bc1cc819effe0c50fc572926faefb6
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-06-30 10:59:14 +0530

    Time: 5 ms (97.29%) | Memory: 44 MB (75.60%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -7,14 +7,6 @@ class Solution {
             cost = _cost;
         }
     }
-    class Tuple{
-        int to, cost, stop;
-        Tuple(int _to, int _cost, int stop) {
-            to = _to;
-            cost = _cost;
-            this.stop = stop;
-        }
-    }
 
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int stops) {
@@ -30,21 +22,24 @@ class Solution {
             adj.get(flight[0]).add(new Pair(flight[1], flight[2]));
         }
 
-        Deque<Tuple> dq = new ArrayDeque<>();
-        dq.offer(new Tuple(src, 0, 0));
+        Deque<Pair> dq = new ArrayDeque<>();
+        dq.offer(new Pair(src, 0));
         dist[src] = 0;
+        int k = 0;
 
         while( !dq.isEmpty() ) {
-            Tuple p1 = dq.pollFirst();
-
-            if(stops < p1.stop) continue;
-
-            for(Pair p : adj.get(p1.to)) {
-                if(dist[p.to] > (p1.cost + p.cost) && p1.stop <= stops) {
-                    dist[p.to] = p1.cost + p.cost;
-                    dq.add(new Tuple(p.to, dist[p.to], p1.stop+1));
+            int sz = dq.size(); 
+            for(int a = 0; a<sz; a++) {
+                Pair p1 = dq.pollFirst();
+                if(stops < k) continue;
+                for(Pair p : adj.get(p1.to)) {
+                    if(dist[p.to] > p1.cost + p.cost) {
+                        dist[p.to] = p1.cost + p.cost;
+                        dq.add(new Pair(p.to, dist[p.to]));
+                    }
                 }
             }
+            k++;
         }
 
         return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
