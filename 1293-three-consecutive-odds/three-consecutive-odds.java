9338e117767f253eca2ecd67dc4426306bee5957
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-07-01 07:04:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (13.08%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int n = arr.length;
+        for(int i = 1; i<n-1; i++) {
+            if(arr[i-1] % 2 != 0 && arr[i] % 2 != 0 && arr[i+1] % 2 != 0) return true;
+        }
+
+        return false;
+    }
+}
