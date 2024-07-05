3a9514603700d12a726419ced693da4d8a6f1c25
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-07-05 22:18:43 +0530

    Time: 18 ms (17.15%) | Memory: 44.1 MB (57.02%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,15 +6,13 @@ class Solution {
         Map<Character, Integer> map = new HashMap<>();
         for(int i=0; i<n; i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)-1);
         }
 
-        final boolean[] flag = {true};
-        map.forEach((key, value) -> {
-            if(value < 0) {
-                flag[0] = false;
-            }
-        });
-        return flag[0];
+        for(int i=0; i<n; i++) {
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0)-1);
+            if(map.get(t.charAt(i)) == 0) map.remove(t.charAt(i));
+        }
+
+        return map.size() == 0;
     }
 }
