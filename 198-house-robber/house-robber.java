3da7e6dad07da4bb237ded2db4bbe3be6aee6d46
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-07-14 12:11:32 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (48.24%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,15 +3,16 @@ class Solution {
 
         int n = nums.length;
         int [] dp = new int[n];
-        dp[0] = nums[0];
-        if(n>=2) dp[1] = nums[1];
-        else return dp[0];
+        Arrays.fill(dp, -1);
 
-        for(int i = 2; i<n; i++) {
-            if(i == 2) dp[i] = nums[i] + dp[0];
-            else dp[i] = Math.max(dp[i-2], dp[i-3]) + nums[i];
-        }
+        return solve(dp,  nums, n-1);
+    }
 
-        return Math.max(dp[n-1], dp[n-2]);
+    public int solve(int[] dp, int nums[], int idx) {
+        if(idx < 0) return 0;
+        if(dp[idx] != -1) return dp[idx];
+
+        return dp[idx] = Math.max(solve(dp, nums, idx-1), solve(dp, nums, idx-2) + nums[idx]);
+
     }
 }
