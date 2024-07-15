9a824f54487459ee742b51d43704763d835001c9
Sai Kiran Varma Pingili <111674572+Saiki1212@users.noreply.github.com>
2024-07-15 13:51:31 +0530

    Time: 82 ms (20.86%) | Memory: 55.7 MB (63.24%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -19,26 +19,19 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for(int node_details[] : des) {
-            if(map.containsKey(node_details[0])) {
-                TreeNode parent = map.get(node_details[0]);
-                if(!map.containsKey(node_details[1])) {
-                    map.put(node_details[1], new TreeNode(node_details[1]));   
-                }
-                set.add(node_details[1]);
-                TreeNode child = map.get(node_details[1]);
-                if(node_details[2] == 0) parent.right = child;
-                else parent.left = child;
-            }
-            else {
-                TreeNode parent = new TreeNode(node_details[0]);
-                map.put(node_details[0], parent);
-                if(!map.containsKey(node_details[1])) {
-                    map.put(node_details[1], new TreeNode(node_details[1]));   
-                }
-                set.add(node_details[1]);
-                TreeNode child = map.get(node_details[1]);
-                if(node_details[2] == 0) parent.right = child;
-                else parent.left = child;
+            int parentVal = node_details[0];
+            int childVal = node_details[1];
+            boolean isLeft = node_details[2] == 1;
+
+            TreeNode parent = map.computeIfAbsent(parentVal, k -> new TreeNode(parentVal));
+            TreeNode child = map.computeIfAbsent(childVal, k -> new TreeNode(childVal));
+
+            set.add(childVal);
+
+            if (isLeft) {
+                parent.left = child;
+            } else {
+                parent.right = child;
             }
         }
         for(int i[] : des) {
